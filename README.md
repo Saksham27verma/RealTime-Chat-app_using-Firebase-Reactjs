d13ee29c97ee27bcb7b8752aa1d59f3587593d05
Saksham Verma <saksham27verma.com>
2024-06-03 20:08:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Real Time Chat Application 📲
+
+## About 
+<p>A real-time chat application built with React.js and Firebase.</p>
+
+## Key Features 
+- **Real-time Messaging :** Enjoy instant communication with Firebase Realtime Database.
+- **Google Authentication :** Enhanced security and convenience for users with seamless Google login.
+- **Exclusive Access :**  Users can enter the chat room only after logging in, ensuring a personalized experience.
+- **Cloud Firestore :**  All messages securely stored for easy retrieval.
+
+## Tech Stack Used 
+- **React.js :** For a responsive user interface.
+- **Firebase :** Powering real-time updates, authentication, and cloud storage.
+- **Google Authentication :** Seamless login.
+- **Cloud Firestore :** Efficient chat message storage.
+
+✨ Exploring Firebase was fun, and integrating Google Authentication added an extra layer of security. Cloud Firestore proved perfect for storing messages securely.
+
+## Screenshots
+
+![Screenshot 2023-12-24 135425](https://github.com/AdityaSingh2005/Real-Time-Chat-Application-Using-ReactJs-Firebase/assets/103613774/b7e5187b-81c5-4259-b5fc-aa805bd2754f)
---
+++ chat-room/src/App.js
@@ -0,0 +1,108 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, limit, onSnapshot } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{color:"golden"}}>Chat Room</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const useSignInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  return (
+    <div>
+      <button className="sign-in" onClick={useSignInWithGoogle}>
+        Sign in with Google
+      </button>
+     <center><b><p style={{color:'#0b93f6'}}>" SignIn to start Chatting "</p></b></center>  
+    </div>
+  );
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = collection(firestore, 'messages');
+  const q = query(messagesRef, orderBy('createdAt'), limit(25));
+
+  const [messages] = useCollectionData(q, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid } = auth.currentUser;
+
+    await addDoc(messagesRef, {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <main>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Typing..." />
+        <button type="submit" disabled={!formValue}>
+        ✒️
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src="user.png" alt='User' />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
